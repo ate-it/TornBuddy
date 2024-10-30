73bcc31faef9b4e9f59e921649db5848f33dbaa7
Your Name <you@example.com>
2024-10-30 13:37:20 +0000

    dump

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,8 +1,18 @@
 class PlayersController < ApplicationController
+  include ApiHelper
   def create
     key = params["player"]["key"]
-    print (key)
+    response =  api_call(2, "user", "", key)
+    # Call Torn's API /user, if we get a good response the key is valid
+
+    if response["error"].blank?
+    # TODO: Create account and loging
+
+    else
+      # TODO: Return error to user and redirect to new
+    end
   end
+
 
   def new
     # TODO: if player is logged in, redirect to dashboard
---
+++ app/helpers/api_helper.rb
@@ -0,0 +1,14 @@
+module ApiHelper
+  def api_call(version, section, selections, key)
+    case version
+    when 2
+      base_url = "https://api.torn.com/v2/"
+    when 1
+      base_url = "https://api.torn.com/"
+    end
+    call_string = base_url+section="?key="+key
+    response = HTTParty.get(call_string)
+
+    JSON.parse(response.body)
+  end
+end
