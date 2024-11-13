b8c16aa0ab87db2bb239b307f7096f550009a51d
Your Name <you@example.com>
2024-11-13 15:37:26 +0000

    loot work

--- loot/models.py
+++ loot/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from TornBuddy.handy import apiCall, romanToInt
+
 
 class NPC(models.Model):
     torn_id = models.IntegerField(unique=True)
@@ -10,8 +12,25 @@ class NPC(models.Model):
     last_update = models.DateTimeField
     visible = models.BooleanField(default=False)
 
-    def update(self, key):
-        print(2)
-        # FIXME: key = random key pluck
-        # TODO: get user info
-        # TODO: update user info
+    def update(self):
+
+        key = "4NCRzvFCTehQNoTC"  # FIXME: Key
+        req = apiCall("user", self.torn_id, "profile,timestamp", key=key)
+
+        if "apiError" in req:
+            return req["apiError"]
+        else:
+
+            self.name = req.get("name", "?")
+            self.last_update = req.get("timestamp")
+
+            states = req.get("states")
+            status = req.get("status")
+
+            self.loot_level = romanToInt(status["details"].replace("Loot level ", ""))
+            if states["hospital_timestamp"] != 0:
+                self.last_hospital = states["hospital_timestamp"]
+                self.status = "hospitalized"
+            else:
+                self.status = status["details"]
+            self.save()
--- loot/views.py
+++ loot/views.py
@@ -5,4 +5,9 @@ from loot.models import NPC
 
 def index(request):
     NPCs = NPC.objects.filter(visible=True)
-    return render(request, "loot/index.html", NPCs)
+    # TODO: Only run this if data is x old
+    # TODO: Run in backround and live update view
+    for npc in NPCs:
+        npc.update()
+    context = {"NPCs": NPCs}
+    return render(request, "loot/index.html", context)
