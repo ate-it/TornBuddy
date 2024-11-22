f7e5f5c5964e3c464e5f62a41f4c473ba7d857cb
Your Name <you@example.com>
2024-11-22 13:22:19 +0000

    Moved django settings to .env

--- TornBuddy/settings.py
+++ TornBuddy/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-o=v5-2==ib&sk&l$cc)@q5g_*%t@8#966#*sldy+$r+qv@+=4r"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
