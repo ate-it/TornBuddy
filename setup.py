b1727625e4df007f9cab794ca11de67f881da112
Your Name <you@example.com>
2024-11-26 14:52:29 +0000

    Added missing NPC

--- setup.py
+++ setup.py
@@ -22,8 +22,10 @@ User.objects.create_superuser("admin", "admin@example.com", "adminpass")
 # NPC's
 print("Create NPC's")
 NPC.objects.create(torn_id=4, name="Duke", visible=True)  # Duke
-NPC.objects.create(torn_id=15, name="Leslie", visible=True)  # Leslie
+NPC.objects.create(torn_id=7, name="Amanda", visible=False)
 NPC.objects.create(torn_id=10, name="Scrooge", visible=False)  # Scrooge
+NPC.objects.create(torn_id=15, name="Leslie", visible=True)  # Leslie
+NPC.objects.create(torn_id=17, name="Easter Bunny", visible=False)  # Leslie
 NPC.objects.create(torn_id=19, name="Jimmy", visible=True)  # Jimmy
 NPC.objects.create(torn_id=20, name="Fernando", visible=True)  # Fernando
 NPC.objects.create(torn_id=21, name="Tiny", visible=True)  # Tiny
