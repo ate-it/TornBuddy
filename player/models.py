9721c503429688fff6886604446710499a6a8d91
Your Name <you@example.com>
2024-12-04 11:28:32 +0000

    Resolves #11

--- player/models.py
+++ player/models.py
@@ -19,12 +19,11 @@ class Player(models.Model):
     age = models.IntegerField(default="0")
     role = models.TextField()
     donator = models.IntegerField(default="0")
-    player_id = models.IntegerField(default="0")
     property_id = models.IntegerField(default="0")
     revivable = models.IntegerField(default="0")
     profile_image = models.TextField()
     life_current = models.IntegerField(default="0")
-    life_maxium = models.IntegerField(default="0")
+    life_maximum = models.IntegerField(default="0")
     life_increment = models.IntegerField(default="0")
     life_interval = models.IntegerField(default="0")
     life_fulltime = models.IntegerField(null=True, default=28377016)
@@ -40,5 +39,44 @@ class Player(models.Model):
     last_action_relative = models.TextField()
 
     def update_own_key(self):
-        # TODO: Update player
+
+        from dateutil import parser
+
+        from TornBuddy.handy import apiCall
+
+        response = apiCall("user", "", "", self.api_key)
+        self.name = response["name"]
+        self.level = response["level"]
+        self.honor = response["honor"]
+        self.gender = response["gender"]
+        self.property = response["property"]
+        self.signup = parser.parse(response["signup"]).timestamp()
+        self.awards = response["awards"]
+        self.friends = response["friends"]
+        self.enemies = response["enemies"]
+        self.forum_posts = response["forum_posts"]
+        self.karma = response["karma"]
+        self.age = response["age"]
+        self.role = response["role"]
+        self.donator = response["donator"]
+        self.torn_id = response["player_id"]
+        self.property_id = response["property_id"]
+        self.revivable = response["revivable"]
+        self.profile_image = response["profile_image"]
+        self.life_current = response["life"]["current"]
+        self.life_maximum = response["life"]["maximum"]
+        self.life_increment = response["life"]["increment"]
+        self.life_interval = response["life"]["interval"]
+        self.life_fulltime = response["life"]["fulltime"]
+        self.status_description = response["status"]["description"]
+        self.status_details = response["status"]["details"]
+        self.status_state = response["status"]["state"]
+        self.status_color = response["status"]["color"]
+        self.status_until = response["status"]["until"]
+        self.states_hospital_timestamp = response["states"]["hospital_timestamp"]
+        self.states_jail_timestamp = response["states"]["jail_timestamp"]
+        self.last_action_status = response["last_action"]["status"]
+        self.last_action_timestap = response["last_action"]["timestamp"]
+        self.last_action_relative = response["last_action"]["relative"]
+        self.save()
         return self
